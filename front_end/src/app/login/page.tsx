aec048e00274e2c507e48059fd3219940b5c86be
harshitnitjsr <harshit26092004@gmail.com>
2024-04-06 03:37:46 +0530

    login form

--- front_end/src/app/login/page.tsx
+++ front_end/src/app/login/page.tsx
@@ -1,7 +1,13 @@
+import { LoginForm } from "@/components/loginform";
+
 import React from "react";
 
-function login() {
-  return <div>login</div>;
+function page() {
+  return (
+    <main className="bg-black/[0.96] bg-grid-white/[0.02]">
+      <LoginForm />
+    </main>
+  );
 }
 
-export default login;
+export default page;
